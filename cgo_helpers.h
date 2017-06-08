5eddde8da4b48ba1e3167ba955bcae3d83011b53
kreon <ivan.agarkov@gmail.com>
2017-06-08 14:41:18 +0300

    fix v2

---
+++ cgo_helpers.h
@@ -0,0 +1,11 @@
+// MIT
+
+// WARNING: This file has automatically been generated on Thu, 08 Jun 2017 11:26:16 UTC.
+// By https://git.io/c-for-go. DO NOT EDIT.
+
+#include "selinux/selinux.h"
+#include <stdlib.h>
+#pragma once
+
+#define __CGOGEN 1
+
